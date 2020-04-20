b3b271f56f15c1acebeedc96c78747095c3b0490
fanyuedong <632296950@qq.com>
2020-04-20 17:24:04 +0800

    webpack

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,6 +9,7 @@ module.exports = {
       { text: 'HTML', link: '/html/' },
       { text: 'JavaScript', link: '/javascript/' },
       { text: 'CSS', link: '/css/' },
+      { text: 'TOOLS', link: '/tools/' },
       { text: 'github', link: 'https://github.com/flonny', target: '_blank', rel: '' },
     ],
     sidebar: {
@@ -24,10 +25,30 @@ module.exports = {
           ]
         }
       ],
+      '/tools/': [
+        {
+          title: '框架工具',
+          collapsable: false,
+          sidebarDepth: 2,
+          children: [
+            '',
+            ['webpack', 'WEBPACk']
+          ]
+        }
+      ],
     }
   },
   plugins: [
     '@vuepress/back-to-top',
+    '@vuepress/last-updated',
+    {
+      transformer: (timestamp, lang) => {
+        // 不要忘了安装 moment
+        const moment = require('moment')
+        moment.locale(lang)
+        return moment(timestamp).fromNow()
+      }
+    },
     ['@vuepress/active-header-links', {
       sidebarLinkSelector: '.sidebar-link',
       headerAnchorSelector: '.header-anchor'
---
+++ docs/tools/webpack.md
@@ -0,0 +1,19 @@
+# webpack
+> 搭建前端工作流，自动构建<br/>
+> 转换es6语法，转换各种语法糖（react, vue），css 预处理编译，自动添加前缀(浏览器的css 兼容)<br/>
+> webpack 是一个现代的JavaScript静态打包工具。运行webpack会根据JavaScript代码之间的依赖关系生成依赖图，webpack 遍历依赖图打包生成一个或多个bundle<br/>
+## webpack 的优点
+- 配置灵活，具备强大的插件扩展能力
+- 是目前市场上占有率最高的前端打包工具
+- 生态成熟丰富, 插件功能丰富
+## webpack 的概念
+ - webpack 默认配置文件时webpck.config.js
+ - entry
+ - output
+ - loader
+ - plugin
+ - mode
+ - 热更新
+ - code spliting
+ - tree shaking
+ ## entry
